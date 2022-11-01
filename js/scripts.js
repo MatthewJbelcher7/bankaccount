af60da503282932f673931b43f2315b1ed28f049
Matthew <MatthewJBelcher7@live.com>
2022-11-01 17:44:35 -0600

    fix addition function
    Co-authored-by: Aitana Shough <aeshough@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // Business Logic
 
 // constructor function
+
+
 function Bank() {
   this.users = {};
   this.currentId = 0;
@@ -16,13 +18,14 @@ Bank.prototype.assignId = function() {
   return this.currentId;
 };
 
-Bank.prototype.addFunds = function(id, deposit) {
-  let funds = bank.users[id].funds
+Bank.prototype.addFunds = function(deposit) {
+  let funds = parseInt(bank.users["1"].funds);
   funds = funds + deposit;
+  return funds
 }
 
 function subFunds(newDeposit) {
-  funds -= newDeposit
+  funds -= newDeposit;
 }
 
 // Business Logic for Bank User Information
@@ -33,11 +36,20 @@ function User(firstName, lastName, funds) {
 }
 User.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
-};
-
+}
 
 // UI Logic
+let bank = new Bank();
+
+function handleFormSubmission(event) {;
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedIntialDeposit = document.querySelector("input#intial-deposit").value;
+  let newUser = new User(inputtedFirstName, inputtedLastName, inputtedIntialDeposit);
+  bank.addUser(newUser);
+}
 
 window.addEventListener("load", function (){
-
-}
+  document.querySelector("form#user").addEventListener("submit", handleFormSubmission);
+});
